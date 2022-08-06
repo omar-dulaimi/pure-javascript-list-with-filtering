1c3740cb71fd298080ef4d02a906679dd13a1d74
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-08-06 21:43:24 +0300

    parameterize api url for future use

--- constants.js
+++ constants.js
@@ -1,4 +1,10 @@
+const rows = 10;
+const firstName = "Omar";
+const lastName = "Dulaimi";
+const categories = ["cats", "code", "people"];
+
 export const constants = {
-  apiUrl:
-    "http://www.filltext.com/?rows=10&fname=omar&lname=dulaimi&category=[%22cats%22,%20%22code%22,%20%22people%22]",
+  apiUrl: `http://www.filltext.com/?rows=${rows}&fname=${firstName}&lname=${lastName}&category=${JSON.stringify(
+    categories
+  )}`,
 };
