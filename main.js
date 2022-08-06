506d9ebd40337292bc62c477571f970e0cfd7c2d
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-08-06 21:02:27 +0300

    implement static categories

--- main.js
+++ main.js
@@ -2,12 +2,29 @@ const items = [
   { fname: "omar", lname: "dulaimi", category: "people" },
   { fname: "omar", lname: "dulaimi", category: "code" },
   { fname: "omar", lname: "dulaimi", category: "people" },
-  { fname: "omar", lname: "dulaimi", category: "code" },
 ];
+
+const categories = ["people", "code", "cats"];
+
+const categoriesWrapper = document.createElement("div");
+categoriesWrapper.classList = "category-wrapper";
+document.getElementById("wrapper").append(categoriesWrapper);
+
+for (const category of categories) {
+  const categoryItem = document.createElement("div");
+  categoryItem.innerText = category;
+  categoryItem.classList = "category";
+  categoryItem.style = "margin-right: 5px;";
+  categoriesWrapper.append(categoryItem);
+}
 
 for (const item of items) {
   const listItem = document.createElement("div");
   listItem.classList = "list-item";
+
+  const category = document.createElement("div");
+  category.innerText = item.category;
+  category.classList = "category";
 
   const nameWrapper = document.createElement("div");
   nameWrapper.classList = "name-wrapper";
@@ -24,10 +41,6 @@ for (const item of items) {
     .slice(0, 1)
     .toUpperCase()}`;
   listItem.prepend(textIcon);
-
-  const category = document.createElement("div");
-  category.innerText = item.category;
-  category.classList = "category";
 
   nameWrapper.append(category);
 
