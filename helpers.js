57ebd030ac7b3a449452ecb1e03da86a51456d01
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-08-06 21:38:33 +0300

    cleanup

---
+++ helpers.js
@@ -0,0 +1,62 @@
+const filterItems = (category) => {
+  const items = document.getElementsByClassName("list-item");
+  for (const item of items) {
+    if (item.children[1].children[1].innerText !== category) {
+      item.style = "display: none;";
+    } else {
+      item.style = "display: block;";
+    }
+  }
+};
+
+export const displayRows = (items) => {
+  for (const item of items) {
+    const listItem = document.createElement("div");
+    listItem.classList = "list-item";
+
+    const category = document.createElement("div");
+    category.innerText = item.category;
+    category.classList = "category";
+
+    const nameWrapper = document.createElement("div");
+    nameWrapper.classList = "name-wrapper";
+
+    const name = document.createElement("div");
+    name.classList = "name";
+    name.innerText = `${item.fname} ${item.lname}`;
+
+    nameWrapper.append(name);
+
+    const textIcon = document.createElement("div");
+    textIcon.classList = "text-icon";
+    textIcon.innerText = `${item.fname.slice(0, 1).toUpperCase()}${item.lname
+      .slice(0, 1)
+      .toUpperCase()}`;
+    listItem.prepend(textIcon);
+
+    nameWrapper.append(category);
+
+    listItem.append(nameWrapper);
+    document.getElementById("list").append(listItem);
+  }
+};
+
+export const displayCategories = (categories) => {
+  const categoriesWrapper = document.createElement("div");
+  categoriesWrapper.classList = "category-wrapper";
+  document.getElementById("wrapper").append(categoriesWrapper);
+
+  for (const category of categories) {
+    const categoryItem = document.createElement("div");
+    categoryItem.innerText = category;
+    categoryItem.classList = "category";
+    categoryItem.style = "margin-right: 5px; cursor: pointer;";
+    categoryItem.onclick = () => filterItems(category);
+    categoriesWrapper.append(categoryItem);
+  }
+};
+
+export const getCategories = (items) => {
+  const categories = [...new Set(items.map((item) => item.category))];
+  return categories;
+};
--- main.js
+++ main.js
@@ -1,71 +1,11 @@
 import { getItems } from "./api.js";
+import { displayCategories, displayRows, getCategories } from "./helpers.js";
 
 let items = [];
 
-const filterItems = (category) => {
-  const items = document.getElementsByClassName("list-item");
-  for (const item of items) {
-    if (item.children[1].children[1].innerText !== category) {
-      item.style = "display: none;";
-    } else {
-      item.style = "display: block;";
-    }
-  }
-};
-
-const displayRows = () => {
-  for (const item of items) {
-    const listItem = document.createElement("div");
-    listItem.classList = "list-item";
-
-    const category = document.createElement("div");
-    category.innerText = item.category;
-    category.classList = "category";
-
-    const nameWrapper = document.createElement("div");
-    nameWrapper.classList = "name-wrapper";
-
-    const name = document.createElement("div");
-    name.classList = "name";
-    name.innerText = `${item.fname} ${item.lname}`;
-
-    nameWrapper.append(name);
-
-    const textIcon = document.createElement("div");
-    textIcon.classList = "text-icon";
-    textIcon.innerText = `${item.fname.slice(0, 1).toUpperCase()}${item.lname
-      .slice(0, 1)
-      .toUpperCase()}`;
-    listItem.prepend(textIcon);
-
-    nameWrapper.append(category);
-
-    listItem.append(nameWrapper);
-    document.getElementById("list").append(listItem);
-  }
-};
-
-const displayCategories = () => {
-  const categories = [...new Set(items.map((item) => item.category))];
-  const categoriesWrapper = document.createElement("div");
-  categoriesWrapper.classList = "category-wrapper";
-  document.getElementById("wrapper").append(categoriesWrapper);
-
-  for (const category of categories) {
-    const categoryItem = document.createElement("div");
-    categoryItem.innerText = category;
-    categoryItem.classList = "category";
-    categoryItem.style = "margin-right: 5px; cursor: pointer;";
-    categoryItem.onclick = () => filterItems(category);
-    categoriesWrapper.append(categoryItem);
-  }
-};
-
-const exec = async () => {
+(async () => {
   items = await getItems();
-
-  displayCategories();
-  displayRows();
-};
-
-exec();
+  const categories = getCategories(items);
+  displayCategories(categories);
+  displayRows(items);
+})();
